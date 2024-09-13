6a5a31b9b6182f3a1ad17b216bd57c724dad2c5b
Asif-Ruhani <asifruhani68@gmail.com>
2024-09-13 15:11:49 +0600

    changes

--- MainPage.js
+++ MainPage.js
@@ -1,6 +1,6 @@
 
 // Create public complain dynamically (for example)
-const CreateComplain = () => {
+const PublicComplain = () => {
     console.log('Sign in button is clicked');
     const PublicComplainDiv = document.getElementById('PublicComplain');
     for (let i = 0; i < 20; i++) {
@@ -12,4 +12,22 @@ const CreateComplain = () => {
         `;
         PublicComplainDiv.appendChild(EachComplainDiv);
     }
+}
+
+
+
+const MyComplain = () =>{
+  const MyComplain=document.getElementById('MyComplain');
+  const MyComplains=document.getElementById('MyComplains');
+
+  for( let i=0; i<8;i++){
+    const Complain=document.createElement('div');
+    Complain.className='MyEachComplain';
+    Complain.innerHTML=`
+            <h1>My complain ${i}</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse delectus maxime vel reprehenderit amet Voluptas minima alias vitae explicabo cumque, ab a adipisci sint eligendi, non sed iste itaque libero.</p>
+    `;
+       MyComplains.appendChild(Complain);
+       MyComplain.appendChild(MyComplains);
+  }
 }
